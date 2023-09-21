b82fdf953c319de15054d2e0ded9b382308c29f9
Smegalex <smegalsuper@gmail.com>
2023-09-21 10:04:21 +0300

    1-13 + call 1-13

--- lab2.py
+++ lab2.py
@@ -36,7 +36,7 @@ def RandomArray(N):
             if arr.count(randomized) < 1:
                 arr.append(randomized)
                 break
-    print(f"Масив рандомних цілих чисел від 1 до N без повторів:\n{arr}")
+    print(f"Масив рандомних цілих чисел від 1 до {N} без повторів:\n{arr}")
 
 #6.
 def arrayReshape():
@@ -68,13 +68,37 @@ def zeroArray():
     print (f"Нульова матриця 10 на 10:\n{zeros}")
 
 #11.
+def smallestAndLargestToVector ():
+    arr = np.array([[15, 8, -2], [36, 1, 0], [9, 12, 4], [6, 29, -3]])
+    print(f"Дано матрицю:\n{arr}")
+    arr = np.split(np.sort(arr, None),2)
+    arrSmall = arr[0]
+    arrBig = arr[1]
+    print(f"Вектор менших чисел:\n{arrSmall}\n\nВектор більших чисел:{arrBig}")
 
+#12. 24 - Обчислити добуток найбільшого й найменшого елемента масива
+def multiplyBiggestAndSmallest(arr):
+    min = np.amin(arr)
+    max = np.amax(arr)
+    multiply = min * max
+    print(f"Добуток найменшого та найбільшого значення масиву {arr}:\n{multiply}")
 
+#13. 
+def onlyOnceWords (arr1, arr2):
+    onlyOnce = []
+    print(f"Дано масиви:\n{arr1}\n{arr2}")
+    for i in arr1:
+        if (arr2.count(i) == 0) & (onlyOnce.count(i) == 0) & (arr1.count(i) == 1):
+            onlyOnce.append(i)
+    for i in arr2:
+        if (arr1.count(i) == 0) & (onlyOnce.count(i) == 0) & (arr2.count(i) == 1):
+            onlyOnce.append(i)
+    print(f"Слова, що були тільки 1 раз:\n{onlyOnce}")
 
+#14.
 
-
-def chooseTask(N):
-    match N:
+def chooseTask(i):
+    match i:
         case 1:
             print("Завдання 1.")
             arrayOneToNineNineNine()
@@ -83,10 +107,48 @@ def chooseTask(N):
             unlimitedNumbersArrification(14, 13, -6, 0, 8, -18)
         case 3:
             print("Завдання 3.")
-            choice = input("Оберіть варіант завдання (1-2):")
+            choice = int(input("Оберіть варіант завдання (1-2):"))
             if choice == 1:
                 arrSmallToLarge([14, 13, -6, 0, 8, -18, 17, 42, -1, 4])
             elif choice == 2:
                 arrLargeToSmall([14, 13, -6, 0, 8, -18, 17, 42, -1, 4])
             else:
                 print("Немає такого варіанту завдання.")
+        case 4:
+            print("Завдання 4.")
+            choice = int(input("Оберіть варіант завдання (1-2):"))
+            if choice == 1:
+                arrTogether([14, 13, -6, 0, 8, -18, 17, 42, -1, 4],[12, -37, -2, 94, 25, 5])
+            elif choice == 2:
+                arrToArr([14, 13, -6, 0, 8, -18, 17, 42, -1, 4],[12, -37, -2, 94, 25, 5])
+            else:
+                print("Немає такого варіанту завдання.")
+        case 5:
+            print("Завдання 5.")
+            RandomArray(int(input("Введіть натуральне число N:")))
+        case 6:
+            print("Завдання 6.")
+            arrayReshape()
+        case 7:
+            print("Завдання 7.")
+            arrToString()
+        case 8:
+            print("Завдання 8.")
+            vectorLineToColumn()
+        case 9:
+            print("Завдання 9.")
+            oneTwoThreeRepeating()
+        case 10:
+            print("Завдання 10.")
+            zeroArray()
+        case 11:
+            print("Завдання 11.")
+            smallestAndLargestToVector ()
+        case 12:
+            print("Завдання 12.")
+            multiplyBiggestAndSmallest([14, 13, -6, 0, 8, -18, 17, 42, -1, 4])
+        case 13:
+            print("Завдання 13.")
+            onlyOnceWords(['кіт', 'в', 'слон', 'виноград', 'мене', 'кіт', 'моцарела', 'день'],['слон', 'народження', 'моцарела', 'виноград'])
+
+chooseTask(int(input("Оберіть завдання (1-18):")))
